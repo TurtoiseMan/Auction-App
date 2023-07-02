eeccf479d932838b4bb47e2da4cd7af717d46762
TurtoiseMan <u1704120@student.cuet.ac.bd>
2023-07-02 13:58:52 +0600

    add js generated navbar

--- client/assets/js/itemBid.js
+++ client/assets/js/itemBid.js
@@ -4,32 +4,38 @@ function handleFormSubmit(event) {
   const bidAmount = document.getElementById("new-bid").value;
 
   const editedItem = { bidAmount };
-
-  fetch(
-    `https://auction-app-cew6.onrender.com/auction-items/item-bid/${itemId}`,
-    {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(editedItem),
-    }
-  )
-    .then((response) => {
-      if (response.ok) {
-        window.location.href = `item-details.html?id=${itemId}`;
-      } else if (response.status === 400) {
-        response.json().then((data) => {
-          //   console.log(data.error);
-          alert(data.error);
-        });
-      } else {
-        throw new Error("Request failed");
+  //   console.log(bidAmount);
+  const currentBid = document.getElementById("current-bid").value;
+  if (bidAmount <= currentBid) {
+    const error = document.getElementById("form-error");
+    error.textContent = "Bid amount should be higher than the current amount.";
+  } else {
+    fetch(
+      `https://auction-app-cew6.onrender.com/auction-items/item-bid/${itemId}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(editedItem),
       }
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+    )
+      .then((response) => {
+        if (response.ok) {
+          window.location.href = `item-details.html?id=${itemId}`;
+        } else if (response.status === 400) {
+          response.json().then((data) => {
+            //   console.log(data.error);
+            // alert(data.error);
+          });
+        } else {
+          throw new Error("Request failed");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 }
 
 const urlParams = new URLSearchParams(window.location.search);
--- client/assets/js/navbar.js
+++ client/assets/js/navbar.js
@@ -1,10 +1,57 @@
+function buildNavbar() {
+  const header = document.getElementsByTagName("header")[0];
+  header.classList.add("navbar");
+  const navbarContainer = document.createElement("div");
+  navbarContainer.classList.add("navbar-container");
+  header.appendChild(navbarContainer);
+
+  const navbarLogo = document.createElement("div");
+  navbarLogo.classList.add("navbar-logo");
+  navbarContainer.appendChild(navbarLogo);
+
+  const a = document.createElement("a");
+  a.href = "index.html";
+  a.textContent = "Auction App";
+  navbarLogo.appendChild(a);
+
+  const hamburger = document.createElement("div");
+  hamburger.classList.add("hamburger");
+  navbarContainer.appendChild(hamburger);
+
+  const hamburgerMenu = document.createElement("button");
+  hamburgerMenu.classList.add("hamburger-menu");
+  hamburger.appendChild(hamburgerMenu);
+
+  for (let i = 0; i < 3; i++) {
+    const span = document.createElement("span");
+    span.classList.add("hamburger-icon");
+    hamburgerMenu.appendChild(span);
+  }
+
+  const dropDown = document.createElement("div");
+  dropDown.classList.add("drop-down");
+  header.appendChild(dropDown);
+
+  const homeLink = document.createElement("a");
+  homeLink.href = "index.html";
+  homeLink.textContent = "Home";
+  dropDown.appendChild(homeLink);
+
+  const addItemLink = document.createElement("a");
+  addItemLink.href = "add-item.html";
+  addItemLink.textContent = "Add Item";
+  dropDown.appendChild(addItemLink);
+}
+
+buildNavbar();
+
 document.addEventListener("DOMContentLoaded", () => {
-    const hamburgerButton = document.querySelector(".hamburger-menu");
-    const dropdownMenu = document.querySelector(".drop-down");
-  
-    hamburgerButton.addEventListener("click", toggleDropdown);
-  
-    function toggleDropdown() {
-      dropdownMenu.classList.toggle("show");
-    }
-  });
+  const hamburgerButton = document.querySelector(".hamburger-menu");
+  const dropdownMenu = document.querySelector(".drop-down");
+
+  hamburgerButton.addEventListener("click", toggleDropdown);
+
+  function toggleDropdown() {
+    dropdownMenu.classList.toggle("show");
+  }
+});
